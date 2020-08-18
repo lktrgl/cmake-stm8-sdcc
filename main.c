824e23fc012be578fbd0eae160f3bb084e085bc3
lktrgl <32882989+lktrgl@users.noreply.github.com>
2020-08-18 23:42:22 +0300

    code clean up

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
-int main(int arc, char** argv)
+#include <custom_state_diagram.h>
+
+int main ( int arc, char** argv )
 {
-    return 0;
+  ( void ) arc;
+  ( void ) argv;
+
+  run_state_machine ( &g_state_diagram_custom );
+
+  return 0;
 }
